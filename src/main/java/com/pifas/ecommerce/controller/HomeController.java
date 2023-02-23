b1a198ad1fed3cc5479f541148de41e007efa24e
Epifanio <pifas412@hotmail.com>
2023-02-23 13:12:47 -0600

    mostrar la vista del producto al usuario

--- src/main/java/com/pifas/ecommerce/controller/HomeController.java
+++ src/main/java/com/pifas/ecommerce/controller/HomeController.java
@@ -1,9 +1,14 @@
 package com.pifas.ecommerce.controller;
 
+import org.slf4j.Logger;
+
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pifas.ecommerce.service.ProductoService;
@@ -12,6 +17,8 @@ import com.pifas.ecommerce.service.ProductoService;
 @RequestMapping("/")
 public class HomeController {
 
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;
 
@@ -19,5 +26,12 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "usuario/home";
+	}
+	
+	
+	@GetMapping("productohome/{id}")
+	public String productoHome(@PathVariable Integer id) {
+		log.info("Id enviado como parámetro{}", id);
+		return"usuario/productoHome";
 	}
 }
