3c9e220b4eb6eba5d87036fcefae24484f7428be
Epifanio <pifas412@hotmail.com>
2023-02-23 15:27:56 -0600

    Validación para doble producto y ver el carrito desde la cabecera desde el template del usuario

--- src/main/java/com/pifas/ecommerce/controller/HomeController.java
+++ src/main/java/com/pifas/ecommerce/controller/HomeController.java
@@ -69,8 +69,15 @@ public class HomeController {
 		detalleOrden.setNombre(producto.getNombre());
 		detalleOrden.setTotal(producto.getPrecio() * cantidad);
 		detalleOrden.setProducto(producto);
-		
-		detalles.add(detalleOrden);
+
+// Validar que el producto no se añada dos veces
+
+		Integer idProducto = producto.getId();
+		boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+		if (!ingresado) {
+			detalles.add(detalleOrden);
+		}
 
 		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
 
@@ -80,5 +87,41 @@ public class HomeController {
 
 		return "usuario/carrito";
 
+	}
+
+	// Quitar un producto del carrito
+
+	@GetMapping("/delete/cart/{id}")
+	public String deleteProductCar(@PathVariable Integer id, Model model) {
+
+		// Lista nueva de productos
+		List<DetalleOrden> ordenesNueva = new ArrayList<DetalleOrden>();
+
+		for (DetalleOrden detalleOrden : detalles) {
+			if (detalleOrden.getProducto().getId() != id) {
+				ordenesNueva.add(detalleOrden);
+			}
+		}
+
+		// Poner la nueva lista con los productos restantes
+		detalles = ordenesNueva;
+
+		double sumaTotal = 0;
+
+		sumaTotal = detalles.stream().mapToDouble(dt -> dt.getTotal()).sum();
+
+		orden.setTotal(sumaTotal);
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+
+		return "usuario/carrito";
+
+	}
+
+	@GetMapping("/getCart")
+	public String getCart(Model model) {
+		model.addAttribute("cart", detalles);
+		model.addAttribute("orden", orden);
+		return "usuario/carrito";
 	}
 }
