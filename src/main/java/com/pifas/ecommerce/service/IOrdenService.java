44434919c4d4b344b10733ce7d85bf613af452d2
Epifanio <pifas412@hotmail.com>
2023-02-25 16:06:06 -0600

    metodo para generar numero de l a orden

--- src/main/java/com/pifas/ecommerce/service/IOrdenService.java
+++ src/main/java/com/pifas/ecommerce/service/IOrdenService.java
@@ -1,8 +1,12 @@
 package com.pifas.ecommerce.service;
 
+import java.util.List;
+
 import com.pifas.ecommerce.model.Orden;
 
 public interface IOrdenService {
+
+	List<Orden> finAll();
 	
 	Orden save (Orden orden);
 	
--- src/main/java/com/pifas/ecommerce/service/OrdenServiceImpl.java
+++ src/main/java/com/pifas/ecommerce/service/OrdenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pifas.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.pifas.ecommerce.model.Orden;
@@ -9,10 +12,42 @@ public class OrdenServiceImpl implements IOrdenService {
 
 	@Autowired
 	private IOrdenRepository ordenRepository;
-	
+
 	@Override
-	public Orden save(Orden orden) {		
+	public Orden save(Orden orden) {
 		return ordenRepository.save(orden);
 	}
 
+	@Override
+	public List<Orden> finAll() {
+		return ordenRepository.findAll();
+	}
+
+	public String generarNumeroOrden() {
+		int numero = 0;
+		String numeroConcatenado = "";
+
+		List<Orden> ordenes = finAll();
+
+		List<Integer> numeros = new ArrayList<Integer>();
+
+		ordenes.stream().forEach(o -> numeros.add(Integer.parseInt(o.getNumero())));
+
+		if (ordenes.isEmpty()) {
+			numero = 1;
+		} else {
+			numero = numeros.stream().max(Integer::compare).get();
+			numero++;
+		}
+
+		if (numero<10) {
+			numeroConcatenado="000000000" + String.valueOf(numero);
+		} else if (numero<100) {
+			numeroConcatenado="00000000" + String.valueOf(numero);
+		}else if(numero<1000) {
+			numeroConcatenado="0000000" + String.valueOf(numero);
+		}
+		
+		return numeroConcatenado;
+	}
 }
