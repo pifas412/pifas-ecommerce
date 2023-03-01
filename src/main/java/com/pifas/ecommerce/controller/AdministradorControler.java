678c608fefef31a2bddac22199323fabaad9e451
Epifanio <pifas412@hotmail.com>
2023-02-28 23:05:12 -0600

    Funcionalidad para ver las ordenes por el Admin

--- src/main/java/com/pifas/ecommerce/controller/AdministradorControler.java
+++ src/main/java/com/pifas/ecommerce/controller/AdministradorControler.java
@@ -9,31 +9,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pifas.ecommerce.model.Producto;
+import com.pifas.ecommerce.service.IOrdenService;
 import com.pifas.ecommerce.service.IUsuarioService;
 import com.pifas.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorControler {
-	
+
 	@Autowired
 	private IUsuarioService usuarioService;
 
 	@Autowired
-	private ProductoService productoService;
+	private IOrdenService ordenService;
 	
+	@Autowired
+	private ProductoService productoService;
+
 	@GetMapping("")
 	public String home(Model model) {
-		List<Producto> productos=productoService.findAll();
-		model.addAttribute("productos",productos);
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos);
 		return "administrador/home";
 	}
 
-	
 	@GetMapping("/usuarios")
 	public String usuarios(Model model) {
 		model.addAttribute("usuarios", usuarioService.findAll());
-		
-		return"administrador/usuarios";
+		return "administrador/usuarios";
+	}
+	
+	@GetMapping("/ordenes")
+	public String ordenes(Model model) {
+		model.addAttribute("ordenes", ordenService.finAll());
+		return"administrador/ordenes";
 	}
 }
