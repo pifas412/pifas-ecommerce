d2d606cad06f2d616fa7a1d97c1581eaf1dc74fb
Epifanio <pifas412@hotmail.com>
2023-02-28 23:30:16 -0600

    Funcionalidad ver detalle de orden desde el Admin

--- src/main/java/com/pifas/ecommerce/controller/AdministradorControler.java
+++ src/main/java/com/pifas/ecommerce/controller/AdministradorControler.java
@@ -2,12 +2,16 @@ package com.pifas.ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pifas.ecommerce.model.Orden;
 import com.pifas.ecommerce.model.Producto;
 import com.pifas.ecommerce.service.IOrdenService;
 import com.pifas.ecommerce.service.IUsuarioService;
@@ -22,10 +26,13 @@ public class AdministradorControler {
 
 	@Autowired
 	private IOrdenService ordenService;
-	
+
 	@Autowired
 	private ProductoService productoService;
 
+	private final Logger logg = LoggerFactory.getLogger(AdministradorControler.class);
+
+	
 	@GetMapping("")
 	public String home(Model model) {
 		List<Producto> productos = productoService.findAll();
@@ -38,10 +45,18 @@ public class AdministradorControler {
 		model.addAttribute("usuarios", usuarioService.findAll());
 		return "administrador/usuarios";
 	}
-	
+
 	@GetMapping("/ordenes")
 	public String ordenes(Model model) {
 		model.addAttribute("ordenes", ordenService.finAll());
-		return"administrador/ordenes";
+		return "administrador/ordenes";
+	}
+	
+	@GetMapping("/detalle/{id}")
+	public String detalle(Model model, @PathVariable Integer id) {
+		logg.info("Id de la orden:{}", id);
+		Orden orden = ordenService.findById(id).get();
+		model.addAttribute("detalles", orden.getDetalle());
+		return"administrador/detalleorden";
 	}
 }
